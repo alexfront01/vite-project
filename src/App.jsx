de08dd685d22937e02c7e15c2b731be4f4fdecba
Aleksey Shmykov <alexfront01@mail.ru>
2025-05-24 21:37:48 +0700

    changed the code in the App.jsx file using createElement

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -9,33 +9,24 @@ function App() {
   const currentYear = currentDate.getFullYear();
   console.log(currentYear);
 
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-	  <div>
-		<h2>Сегодня {currentYear} год</h2>
-	  </div>
-    </>
+	React.createElement(React.Fragment,
+		null,
+		React.createElement('div', null,
+			React.createElement('a', {href:"https://vite.dev", target:"_blank"},
+				React.createElement('img', {src: viteLogo, className: 'logo', alt: "Vite logo"}) ),
+			React.createElement('a', {href:"https://react.dev", target:"_blank"},
+				React.createElement('img', {src: reactLogo, className: 'logo react', alt: "React logo"}) ),
+			),
+		React.createElement('h1', null, 'Vite + React'),
+		React.createElement('div', {className: 'card'},
+			React.createElement('button', {onClick: ()=> setCount((count) => count + 1)}, 'count is' + count),
+			React.createElement('p', null, 'Edit', React.createElement('code', null, 'src/App.jsx'), 'and save to test HMR')
+		),
+		React.createElement('p', {className: 'read-the-docs'}, 'Click on the Vite and React logos to learn more'),
+		React.createElement('div', null,
+			React.createElement('h2', null, `Сегодня ${currentYear} год!`))
+	)
   )
 };
 
